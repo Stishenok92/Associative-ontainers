6914767763b1ec9cd891666a8d73d5ab86fcfd29
Stishenok92 <Stishenok92@gmail.com>
2020-07-05 21:16:32 +0300

    Initial Commit

---
+++ AssociativeСontainers/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AssociativeСontainers
+//
+//  Created by Vadim Stishenok on 05.07.2020.
+//  Copyright © 2020 Vadim Stishenok. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
